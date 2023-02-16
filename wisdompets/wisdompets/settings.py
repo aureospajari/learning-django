251893dcefbb1cc2af9803b82b0a62f0b7940347
aureospajari <aureo.spajari@gmail.com>
2023-02-16 10:16:16 -0300

    securing key

--- wisdompets/wisdompets/settings.py
+++ wisdompets/wisdompets/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os.path
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cyqapi6o#7cv6qiudt5b&f^6f%@ws4vwi(a1vheo85pj2nbxo('
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
